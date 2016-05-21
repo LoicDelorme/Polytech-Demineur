5c204ffbac6acd7f843b11e364ff959668cef4ae
DELORME Loïc <loic.delorme.pro@gmail.com>
2016-05-21 11:49:03 +0200

    Add cell type enumeration

---
+++ src/fr/polytech/demineur/model/CellType.java
@@ -0,0 +1,65 @@
+package fr.polytech.demineur.model;
+
+/**
+ * This enumeration represents a cell type.
+ *
+ * @author DELORME Loïc
+ * @since 1.0.0
+ */
+public enum CellType
+{
+    /**
+     * The cell is empty.
+     */
+	EMPTY,
+
+	/**
+	 * The cell is a mine.
+	 */
+	MINE,
+
+	/**
+	 * The cell is a mine which has exploded.
+	 */
+	MINE_EXPLODED,
+
+	/**
+	 * The cell has a value of 1.
+	 */
+	ONE,
+
+	/**
+	 * The cell has a value of 2.
+	 */
+	TWO,
+
+	/**
+	 * The cell has a value of 3.
+	 */
+	THREE,
+
+	/**
+	 * The cell has a value of 4.
+	 */
+	FOUR,
+
+	/**
+	 * The cell has a value of 5.
+	 */
+	FIVE,
+
+	/**
+	 * The cell has a value of 6.
+	 */
+	SIX,
+
+	/**
+	 * The cell has a value of 7.
+	 */
+	SEVEN,
+
+	/**
+	 * The cell has a value of 8.
+	 */
+	EIGHT;
+}
