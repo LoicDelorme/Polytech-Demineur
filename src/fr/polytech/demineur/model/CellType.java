e63df699722465e234a9cb4f2563da81a370e00a
DELORME Loïc <loic.delorme.pro@gmail.com>
2016-05-21 15:34:41 +0200

    Add image path

--- src/fr/polytech/demineur/model/CellType.java
+++ src/fr/polytech/demineur/model/CellType.java
@@ -11,50 +11,76 @@ public enum CellType
     /**
      * The cell is empty.
      */
-	EMPTY,
+	EMPTY(""),
 
 	/**
 	 * The cell is a mine.
 	 */
-	MINE,
+	MINE("/fr/polytech/demineur/view/resources/mine.png"),
 
 	/**
 	 * The cell has a value of 1.
 	 */
-	ONE,
+	ONE("/fr/polytech/demineur/view/resources/1.png"),
 
 	/**
 	 * The cell has a value of 2.
 	 */
-	TWO,
+	TWO("/fr/polytech/demineur/view/resources/2.png"),
 
 	/**
 	 * The cell has a value of 3.
 	 */
-	THREE,
+	THREE("/fr/polytech/demineur/view/resources/3.png"),
 
 	/**
 	 * The cell has a value of 4.
 	 */
-	FOUR,
+	FOUR("/fr/polytech/demineur/view/resources/4.png"),
 
 	/**
 	 * The cell has a value of 5.
 	 */
-	FIVE,
+	FIVE("/fr/polytech/demineur/view/resources/5.png"),
 
 	/**
 	 * The cell has a value of 6.
 	 */
-	SIX,
+	SIX("/fr/polytech/demineur/view/resources/6.png"),
 
 	/**
 	 * The cell has a value of 7.
 	 */
-	SEVEN,
+	SEVEN("/fr/polytech/demineur/view/resources/7.png"),
 
 	/**
 	 * The cell has a value of 8.
 	 */
-	EIGHT;
+	EIGHT("/fr/polytech/demineur/view/resources/8.png");
+
+	/**
+	 * The image path.
+	 */
+	private final String imagePath;
+
+	/**
+	 * Private constructor.
+	 * 
+	 * @param imagePath
+	 *            The image path.
+	 */
+	private CellType(String imagePath)
+	{
+		this.imagePath = imagePath;
+	}
+
+	/**
+	 * Get the image path.
+	 * 
+	 * @return The image path.
+	 */
+	public String getImagePath()
+	{
+		return this.imagePath;
+	}
 }
